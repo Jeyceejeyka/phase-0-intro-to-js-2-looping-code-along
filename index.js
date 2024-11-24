7577c4a8bb09a860b4615b69cf99fc36796447a8
jeycee <jeyceejeyka635@gmail.com>
2024-11-24 21:15:52 +0300

    Adds for and loop

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const names = ["Elvis", "Kevin", "Jey","Boba"];
+function writeCards(names) {
+    const messages = []; // array to hold the new array content
+  
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+  
+    return messages; // new  array of messages
+  }
+  console.log(writeCards(names))
+
+function countDown(number) {
+    let i = number
+    while(i >= 0) {
+        console.log(i)
+        i--;
+    }
+}
+console.log(countDown(10));
+
+
